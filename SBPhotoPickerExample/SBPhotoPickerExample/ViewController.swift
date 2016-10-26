d649e2f61250210928baa642bec934872fee6aed
songbihai <759464290@qq.com>
2016-10-26 10:45:46 +0800

    first

---
+++ SBPhotoPickerExample/SBPhotoPickerExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SBPhotoPickerExample
+//
+//  Created by 宋碧海 on 16/10/26.
+//  Copyright © 2016年 songbihai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SBPhotoPickerExample/SBPhotoPickerExampleTests/SBPhotoPickerExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  SBPhotoPickerExampleTests.swift
+//  SBPhotoPickerExampleTests
+//
+//  Created by 宋碧海 on 16/10/26.
+//  Copyright © 2016年 songbihai. All rights reserved.
+//
+
+import XCTest
+@testable import SBPhotoPickerExample
+
+class SBPhotoPickerExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
